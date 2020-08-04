1e38537a61d37e83801063ea36126d375b9c0629
yf <yf@Han-MacBook-Pro-1152.local>
2020-08-04 11:57:36 +0800

    init

---
+++ PodTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by yf on 2020/8/4.
+//  Copyright © 2020 anker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
